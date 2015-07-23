3ceae874be1dd1580481200a58a7f6a2c2e8e548
wmk26 <1549359482@qq.com>
2015-07-23 20:11:13 +0800

    atoi

---
+++ atoi/atoi.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<climits>
+using namespace std;
+
+class Solution {
+	public:
+		int myAtoi(string str) {
+			int num = 0;
+			int sign = 1;
+			const int n = str.size();
+			int i = 0;
+			while(str[i] == ' ' && i < n) 
+				i++;
+			if(str[i] == '+') {
+				i++;
+			} else if(str[i] == '-'){
+				sign = -1;
+				i++;
+			}
+
+			for( ; i < n; i++ ) {
+				if(str[i] < '0' || str[i] > '9')
+					break;
+				if(num > INT_MAX / 10 || 
+						(num == INT_MAX/10 && (str[i] - '0') > INT_MAX % 10))
+				{
+					return sign == -1 ? INT_MIN : INT_MAX; 
+				}
+				num = num * 10 + str[i] - '0';
+			}
+			return num * sign;
+		}
+};
+
+int main() {
+	Solution s;
+	string str = "+232xf213";
+	int output = s.myAtoi(str);
+	cout << output << endl;
+	return 0;
+}
