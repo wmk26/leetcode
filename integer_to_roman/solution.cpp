7a6c97d53a3eaf22514953e65bcb5b396ca4928c
wmk26 <1549359482@qq.com>
2015-08-05 17:07:47 +0800

    integer_to_roman

---
+++ integer_to_roman/solution.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+	public:
+		string intToRoman(int num) {
+			const int radix[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+			const string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+			string roman;
+
+			for(size_t i = 0; num > 0; ++i) {
+				int count = num / radix[i];
+				num %= radix[i];
+				for(; count > 0; --count) 
+					roman += symbol[i];
+			}
+			return roman;
+		} 
+};
+
+int main() {
+	Solution s;
+	int num = 3456;
+	cout << s.intToRoman(num) << endl;
+	return 0;
+}
